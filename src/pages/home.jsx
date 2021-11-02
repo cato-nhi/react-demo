5dc8344cf7b318460cf4f1831e34dbead2df38f9
Cato Skogholt <cato.skogholt@nhi.no>
2021-11-02 12:03:44 +0100

    misc fixes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 export default function Home() {
   return (
     <section>
-      <p>Demo contents, in order...</p>
+      <p>React is different than JQuery in the sense that it is a UI component management library as opposed to a utility library wrapping browser APIs.</p>
+      <p>React works through the “virtual DOM”, whereas jQuery interacts with the DOM directly. The virtual DOM is a DOM implementation in memory that compares to the existing DOM elements and makes the necessary changes/updates. And that leads to much faster performance.</p>
+      <p>JQuery can also create elements, but this is not its primary function and the framework is not as efficient with it compared to React.</p>
       <ol>
         <li>
           <a href="file:///C:/code/react-demo/public/javascript.html">
@@ -12,6 +14,9 @@ export default function Home() {
         </li>
         <li>
           <a href="file:///C:/code/react-demo/public/jquery.html">JQuery</a>
+        </li>
+        <li>
+          A simple component (this page)
         </li>
         <li>
           <Link to="no-jsx">No JSX</Link>
--- src/pages/no-jsx.jsx
+++ src/pages/no-jsx.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 
 export default function NoJSX() {
-  const h1 = React.createElement("h1", { children: "Header" })
-  const p = React.createElement("h1", { children: "Lorem ipsum paragraph" })
+  const h1 = React.createElement("h1", { key: 1, children: "Header" })
+  const p = React.createElement("h1", { key: 2, children: "Lorem ipsum paragraph" })
   const wrapper = React.createElement("section", { children: [h1, p] })
 
   return wrapper
